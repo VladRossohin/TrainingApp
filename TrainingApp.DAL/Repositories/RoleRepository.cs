7524e970a40fb5a8f9305e5cb02e26b590e7cbb0
VladRossohin <vladislav.rossohin@gmail.com>
2020-05-01 12:11:17 +0300

    add role repository

---
+++ TrainingApp.DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrainingApp.DAL.Interfaces;
+using TrainingApp.DAL.Models;
+
+namespace TrainingApp.DAL.Repositories
+{
+    public class RoleRepository : IRepository<Role> 
+    {
+        private readonly TrainingDBContext Database;
+
+        public RoleRepository(TrainingDBContext database)
+        {
+            Database = database;
+        }
+
+        public void DeleteItem(Role item)
+        {
+            Database.Role.Remove(item);
+            Database.SaveChangesAsync();
+        }
+
+        public Role GetItem(int? id)
+        {
+            var role = Database.Role.Include("User").Where(r => r.Id == id).FirstOrDefault();
+
+            return role;
+        }
+
+        public void SaveItem(Role item)
+        {
+            Database.Role.Add(item);
+            Database.SaveChangesAsync();
+        }
+
+        public void UpdateItem(Role item)
+        {
+            Database.Entry(item).State = EntityState.Modified;
+            Database.Role.Update(item);
+
+            Database.SaveChangesAsync();
+        }
+    }
+}
