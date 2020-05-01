13d509a739f2420edc0f0844c309854c395ab39a
VladRossohin <vladislav.rossohin@gmail.com>
2020-05-01 12:05:54 +0300

    init user repository

---
+++ TrainingApp.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingApp.DAL.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetItem(int? id);
+        void UpdateItem(T item);
+        void SaveItem(T item);
+        void DeleteItem(T item);
+
+    }
+}
---
+++ TrainingApp.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrainingApp.DAL.Interfaces;
+using TrainingApp.DAL.Models;
+
+namespace TrainingApp.DAL.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly TrainingDBContext Database;
+
+        public UserRepository(TrainingDBContext dBContext)
+        {
+            this.Database = dBContext;
+        }
+
+        public void DeleteItem(User item)
+        {
+            Database.User.Remove(item);
+
+            Database.SaveChangesAsync();
+        }
+
+        public User GetItem(int? id)
+        {
+            var user = Database.User.Include("Role").Include("Training").Where(user => user.Id == id.Value).FirstOrDefault();
+
+            return user;
+        }
+
+        public void SaveItem(User item)
+        {
+            Database.User.Add(item);
+
+            Database.SaveChangesAsync();
+        }
+
+        public void UpdateItem(User item)
+        {
+            Database.Entry(item).State = EntityState.Modified;
+
+            Database.User.Update(item);
+
+            Database.SaveChangesAsync();
+        }
+    }
+}
