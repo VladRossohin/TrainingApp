0da75d4989b17217d6aff26b89b32b86df889be1
VladRossohin <vladislav.rossohin@gmail.com>
2020-05-09 14:11:30 +0300

    mapping config

--- TrainingApp.PL/Util/MappingConfiguration.cs
+++ TrainingApp.PL/Util/MappingConfiguration.cs
@@ -14,19 +14,18 @@ namespace TrainingApp.Api.Util
         public MappingConfiguration()
         {
             CreateMap<User, UserDTO>().ReverseMap();
-
             CreateMap<Sensor, SensorDTO>().ReverseMap();
-
             CreateMap<Training, TrainingDTO>().ReverseMap();
-
             CreateMap<Exercise, ExerciseDTO>().ReverseMap();
-
             CreateMap<Role, RoleDTO>().ReverseMap();
-
             CreateMap<Kick, KickDTO>().ReverseMap();
 
             CreateMap<UserDTO, UserModel>().ReverseMap();
-
+            CreateMap<RoleDTO, RoleModel>().ReverseMap();
+            CreateMap<SensorDTO, SensorModel>().ReverseMap();
+            CreateMap<TrainingDTO, TrainingModel>().ReverseMap();
+            CreateMap<ExerciseDTO, ExerciseModel>().ReverseMap();
+            CreateMap<KickDTO, KickModel>().ReverseMap();
         }
     }
 }
